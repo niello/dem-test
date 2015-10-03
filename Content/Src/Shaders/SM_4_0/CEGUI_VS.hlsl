f9c5fc3dd8279245b1de171979a810c6fe74a137
niello <niello@bk.ru>
2015-10-03 11:58:35 +0300

    New effect framework test files (real CEGUI effect)

---
+++ Content/Src/Shaders/SM_4_0/CEGUI_VS.hlsl
@@ -0,0 +1,29 @@
+
+struct PSSceneIn
+{
+	float4 Pos: SV_Position;
+	float4 Colour: COLOR;
+	float2 Tex: TEXCOORD;
+};
+
+matrix WorldMatrix;
+matrix ProjectionMatrix;
+
+struct VSSceneIn
+{
+	float3 Pos: POSITION;
+	float4 Colour: COLOR;
+	float2 Tex: TEXCOORD;
+};
+
+PSSceneIn VSMain(VSSceneIn In)
+{
+	PSSceneIn Out = (PSSceneIn)0.0;
+
+	Out.Pos = mul(float4(In.Pos, 1), WorldMatrix);
+	Out.Pos = mul(Out.Pos, ProjectionMatrix);
+	Out.Tex = In.Tex;
+	Out.Colour.rgba = In.Colour.bgra;
+
+	return Out;
+}
