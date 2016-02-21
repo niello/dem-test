fe12206460dde4172d503381fe7f3a1cf6fe58f2
niello <niello@bk.ru>
2016-02-21 14:48:49 +0200

    SM4.0 CEGUI effect removed, stub PBR effect added instead

---
+++ Content/Src/Shaders/SM_4_0/PBR.hlsl
@@ -0,0 +1,47 @@
+
+struct VSSceneIn
+{
+	float3 Pos: POSITION;
+	float2 Tex: TEXCOORD;
+};
+
+struct PSSceneIn
+{
+	float4 Pos: SV_Position;
+	float2 Tex: TEXCOORD;
+};
+
+cbuffer ChangeOnResize: register(c0)	// Camera data
+{
+	matrix ProjectionMatrix;
+}
+
+cbuffer ChangePerObject: register(c1)	// Instance data
+{
+	matrix WorldMatrix;
+}
+
+PSSceneIn VSMain(VSSceneIn In)
+{
+	PSSceneIn Out = (PSSceneIn)0.0;
+
+	Out.Pos = mul(float4(In.Pos, 1), WorldMatrix);
+	Out.Pos = mul(Out.Pos, ProjectionMatrix);
+	Out.Tex = In.Tex;
+
+	return Out;
+}
+
+Texture2D TexAlbedo;
+sampler LinearSampler;
+
+float4 PSMain(PSSceneIn In): SV_Target
+{
+	#if DEM_LIGHT_COUNT == 0
+	return float4(0.5f, 0.5f, 0.5f, 1.f);
+	#elif DEM_LIGHT_COUNT == 1
+	return float4(0.8f, 0.8f, 0.8f, 1.f);
+	#else
+	return TexAlbedo.Sample(LinearSampler, In.Tex);
+	#endif
+}
