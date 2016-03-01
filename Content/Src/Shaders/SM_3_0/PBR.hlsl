c4c0669db139d56a85cda9c7d70c7de1435457fa
niello <niello@bk.ru>
2016-03-01 23:04:49 +0200

    SM3.0 sampler array test case

--- Content/Src/Shaders/SM_3_0/PBR.hlsl
+++ Content/Src/Shaders/SM_3_0/PBR.hlsl
@@ -12,39 +12,60 @@ struct PSSceneIn
 };
 
 matrix ProjectionMatrix: register(c0);
+matrix WorldMatrix: register(c6);
 
 struct CData
 {
-	int LoopCount;
-	matrix WorldMatrix; //: register(c8);
+	float4x4 W;
+	float2 mx;
+	float2 m;
 };
 
 struct CData2
 {
+	float3 tt;
 	CData D;
-	float4 XX[3];
 };
 
-CData2 UniformStruct;
+CData2 UniformStruct[2];
 float4 Y[2];
+
+struct CCounters
+{
+	int3 LoopCount[2];
+	int LoopCount2;
+};
+CCounters Counters;
+bool IsFirst;
 
 PSSceneIn VSMain(VSSceneIn In)
 {
 	PSSceneIn Out = (PSSceneIn)0.0;
 
-	Out.Pos = mul(float4(In.Pos, 1), UniformStruct.D.WorldMatrix);
+	Out.Pos = mul(float4(In.Pos, 1), WorldMatrix);
 	Out.Pos = mul(Out.Pos, ProjectionMatrix);
 	Out.Tex = In.Tex;
-	for (int i = 0; i < UniformStruct.D.LoopCount; ++i)
+	if (IsFirst)
 	{
-		Out.Tex = Out.Tex * 0.5f + UniformStruct.XX[2].zw;
+		for (int i = 0; i < Counters.LoopCount[1].y; ++i)
+		{
+			Out.Tex = Out.Tex * 0.5f + UniformStruct[1].tt.zx;
+		}
+	}
+	else
+	{
+		for (int i = 0; i < Counters.LoopCount2; ++i)
+		{
+			Out.Tex = Out.Tex * 0.25f + UniformStruct[1].D.W[3].wz + UniformStruct[1].D.m;
+		}
 	}
 
 	return Out;
 }
 
 Texture2D TexAlbedo;
-sampler LinearSampler { Texture = TexAlbedo; };
+Texture2D TexAlbedo2;
+sampler LinearSampler[2] { { Texture = TexAlbedo2; }, { Texture = TexAlbedo; } };
 
 float4 PSMain(PSSceneIn In): COLOR
 {
@@ -53,6 +74,6 @@ float4 PSMain(PSSceneIn In): COLOR
 #elif DEM_LIGHT_COUNT == 1
 	return float4(0.75f, Y[1].y, 1.f, 1.f);
 #else
-	return tex2D(LinearSampler, In.Tex);
+	return tex2D(LinearSampler[0], In.Tex) * tex2D(LinearSampler[1], In.Tex);
 #endif
 }
