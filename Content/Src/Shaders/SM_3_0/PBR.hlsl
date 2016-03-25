890df3f5dfd8a986fa9ba1b30d754280051f937c
niello <niello@bk.ru>
2016-03-25 19:21:06 +0200

    Stub effect and materials loaded

--- Content/Src/Shaders/SM_3_0/PBR.hlsl
+++ Content/Src/Shaders/SM_3_0/PBR.hlsl
@@ -11,69 +11,22 @@ struct PSSceneIn
 	float2 Tex: TEXCOORD;
 };
 
-matrix ProjectionMatrix: register(c0);
-matrix WorldMatrix: register(c6);
-
-struct CData
-{
-	float4x4 W;
-	float2 mx;
-	float2 m;
-};
-
-struct CData2
-{
-	float3 tt;
-	CData D;
-};
-
-CData2 UniformStruct[2];
-float4 Y[2];
-
-struct CCounters
-{
-	int3 LoopCount[2];
-	int LoopCount2;
-};
-CCounters Counters;
-bool IsFirst;
+matrix ViewProj: register(c0);
+matrix WorldMatrix: register(c4);
 
 PSSceneIn VSMain(VSSceneIn In)
 {
 	PSSceneIn Out = (PSSceneIn)0.0;
-
 	Out.Pos = mul(float4(In.Pos, 1), WorldMatrix);
-	Out.Pos = mul(Out.Pos, ProjectionMatrix);
+	Out.Pos = mul(Out.Pos, ViewProj);
 	Out.Tex = In.Tex;
-	if (IsFirst)
-	{
-		for (int i = 0; i < Counters.LoopCount[1].y; ++i)
-		{
-			Out.Tex = Out.Tex * 0.5f + UniformStruct[1].tt.zx;
-		}
-	}
-	else
-	{
-		for (int i = 0; i < Counters.LoopCount2; ++i)
-		{
-			Out.Tex = Out.Tex * 0.25f + UniformStruct[1].D.W[3].wz + UniformStruct[1].D.m;
-		}
-	}
-
 	return Out;
 }
 
-Texture2D TexAlbedo;
-Texture2D TexAlbedo2;
-sampler LinearSampler[2] { { Texture = TexAlbedo2; }, { Texture = TexAlbedo; } };
+//Texture2D TexAlbedo;
+//sampler LinearSampler { Texture = TexAlbedo; };
 
 float4 PSMain(PSSceneIn In): COLOR
 {
-#if DEM_LIGHT_COUNT == 0
-	return float4(0.5f, 0.5f, Y[1].x, 1.f);
-#elif DEM_LIGHT_COUNT == 1
-	return float4(0.75f, Y[1].y, 1.f, 1.f);
-#else
-	return tex2D(LinearSampler[0], In.Tex) * tex2D(LinearSampler[1], In.Tex);
-#endif
+	return float4(0.95f, 0.95f, 0.1f, 1.f); //tex2D(LinearSampler, In.Tex)
 }
--- Content/Src/Shaders/SM_4_0/PBR.hlsl
+++ Content/Src/Shaders/SM_4_0/PBR.hlsl
@@ -11,60 +11,35 @@ struct PSSceneIn
 	float2 Tex: TEXCOORD;
 };
 
-cbuffer ChangeOnResize: register(c0)	// Camera data
+cbuffer CameraParams: register(c0)
 {
-	matrix ProjectionMatrix;
+	matrix ViewProj;
+	//float3 Eye;
 }
 
-cbuffer ChangePerObject: register(c1)	// Instance data
+cbuffer InstanceParams: register(c1)
 {
 	matrix WorldMatrix;
 }
 
-tbuffer ChangePerObject: register(t0)	// Instance data (skinning)
+tbuffer SkinParams: register(t0)
 {
-	int xx[5];
 	matrix SkinMatrix[1024];
 }
-
-struct CLight
-{
-	float4	Params1;
-	int		Type;
-	bool	CastShadow;
-};
-
-StructuredBuffer<CLight> LightBuffer[2]: register(t3);
-float4 Y[2];
 
 PSSceneIn VSMain(VSSceneIn In)
 {
 	PSSceneIn Out = (PSSceneIn)0.0;
-
 	Out.Pos = mul(float4(In.Pos, 1), WorldMatrix);
-	Out.Pos = mul(Out.Pos, SkinMatrix[1023]);
-	Out.Pos = mul(Out.Pos, ProjectionMatrix);
+	Out.Pos = mul(Out.Pos, ViewProj);
 	Out.Tex = In.Tex;
-	
-	if (LightBuffer[1][2].CastShadow)
-	{
-		Out.Tex = pow(abs(Out.Tex * LightBuffer[0][2].Params1.zw + Y[1].xw), xx[4]);
-	}
-
 	return Out;
 }
 
-Texture2D TexAlbedo[2];
-Texture2DArray TexArray[2];
-sampler LinearSampler[2];
+//Texture2D TexAlbedo;
+//sampler LinearSampler;
 
 float4 PSMain(PSSceneIn In): SV_Target
 {
-#if DEM_LIGHT_COUNT == 0
-	return float4(0.5f, 0.5f, 0.5f, 1.f);
-#elif DEM_LIGHT_COUNT == 1
-	return float4(0.75f, 0.75f, 0.75f, 1.f);
-#else
-	return TexAlbedo[0].Sample(LinearSampler[1], In.Tex) * TexArray[1].Sample(LinearSampler[1], float3(In.Tex, 5)) * TexAlbedo[1].Sample(LinearSampler[0], In.Tex);
-#endif
+	return float4(0.95f, 0.95f, 0.1f, 1.f); //TexAlbedo.Sample(LinearSampler, In.Tex)
 }
