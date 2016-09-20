3a2e3aea2ed6711f06598df6160e61869f740bf0
niello <niello@bk.ru>
2016-09-20 10:58:22 +0200

    Cook-Torrance specular implemented with GGX and Schlick Fresnel term. Not integrated.

--- Content/Src/Shaders/USM/Lighting.hlsl
+++ Content/Src/Shaders/USM/Lighting.hlsl
@@ -16,8 +16,8 @@
 #define LIGHT_TYPE_POINT	1
 #define LIGHT_TYPE_SPOT		2
 
-#define Attenuation			AttenuationQuad
-#define SpotlightFalloff	SpotlightFalloffLinear
+#define AttenuationFunction			AttenuationQuad
+#define SpotlightFalloffFunction	SpotlightFalloffLinear
 
 // Normal mapping
 
@@ -76,9 +76,12 @@ float AttenuationLinear(float Distance, float InvRange)
 // The most realistic
 float AttenuationQuad(float Distance, float InvRange)
 {
+	//return saturate(1.f - SqDistance * InvRange * InvRange);
+
 	float Factor = Distance * InvRange;
-	return saturate(1 - Factor * Factor);
-	// return 1 / (Distance * Distance); - not normalized
+	return saturate(1.f - Factor * Factor);
+
+	// return 1.f / SqDistance; - not normalized
 }
 //---------------------------------------------------------------------
 
@@ -120,19 +123,19 @@ float DiffuseLambert(float3 N, float3 L)
 //---------------------------------------------------------------------
 
 // An useful table with roughness values: https://wiki.blender.org/index.php/User:Guiseppe/Oren_Nayar
-float DiffuseOrenNayar(float3 N, float3 L, float3 V, float Roughness)
+//???send dots as args? saturate?
+float DiffuseOrenNayar(float3 N, float3 L, float3 V, float SqRoughness)
 {
 	float NdotL = dot(N, L);
 	float ClampedNdotL = max(NdotL, 0.f);
 
 	// A = 1, B = 0 - simplifies to lambertian model
-	if (Roughness == 0) return ClampedNdotL;
+	if (SqRoughness == 0) return ClampedNdotL;
 
 	float NdotV = dot(N, V);
 
-	float SqRgh = Roughness * Roughness; //???pre-square?
-	float A = 1.0f - 0.5f * (SqRgh / (SqRgh + 0.57f));
-	float B = 0.45f * (SqRgh / (SqRgh + 0.09f));
+	float A = 1.0f - 0.5f * (SqRoughness / (SqRoughness + 0.57f));
+	float B = 0.45f * (SqRoughness / (SqRoughness + 0.09f));
 
 	float Gamma = dot(V - N * NdotV, L - N * NdotL);
 
@@ -147,3 +150,84 @@ float DiffuseOrenNayar(float3 N, float3 L, float3 V, float Roughness)
 }
 //---------------------------------------------------------------------
 
+// Dot products must be saturated
+float GeometricSmithGGX(float SqRoughness, float NdotV, float NdotL)
+{
+	float SqRoughness2 = SqRoughness * SqRoughness;
+	float OneSubSqRoughness2 = 1.0f - SqRoughness2;
+	float2 Dots = float2(NdotV, NdotL);
+	float2 G = (2.0f * Dots) / (Dots + sqrt(SqRoughness2 + OneSubSqRoughness2 * Dots * Dots));
+	return G.x * G.y;
+}
+//---------------------------------------------------------------------
+
+// Dot products must be saturated
+float GeometricSmithSchlickGGX(float SqRoughness, float NdotV, float NdotL)
+{
+	float K = SqRoughness * 0.5f;
+	float OneSubK = 1.0f - K;
+	float2 Dots = float2(NdotV, NdotL);
+	float2 G = Dots / (Dots * OneSubK + K);
+	return G.x * G.y;
+}
+//---------------------------------------------------------------------
+
+// For conductors
+// Dot products must be saturated
+float3 FresnelSchlick(float3 SpecularColor, float VdotH)
+{
+	return SpecularColor + (1.0f - SpecularColor) * pow((1.0f - VdotH), 5);
+}
+//---------------------------------------------------------------------
+
+// For dielectrics
+// Dot products must be saturated
+float FresnelSchlickSingleChannel(float SpecularColor, float VdotH)
+{
+	return SpecularColor + (1.0f - SpecularColor) * pow((1.0f - VdotH), 5);
+}
+//---------------------------------------------------------------------
+
+//???is correct? produces strange results in PBRViewer!
+// Dot products must be saturated
+float3 FresnelCookTorrance(float3 SpecularColor, float VdotH)
+{
+	float3 SqrtSpecularColor = sqrt(SpecularColor);
+	float3 n = (1.0f + SqrtSpecularColor) / (1.0f - SqrtSpecularColor);
+	float c = VdotH;
+	float3 g = sqrt(n * n + c * c - 1.0f);
+
+	float3 GaddC = g + c;
+	float3 GsubC = g - c;
+
+	float3 part1 = GsubC / GaddC;
+	float3 part2 = (GaddC * c - 1.0f) / (GsubC * c + 1.0f);
+
+	return max(0.0f.xxx, 0.5f * part1 * part1 * ( 1 + part2 * part2));
+}
+//---------------------------------------------------------------------
+
+// Dot products must be saturated
+float NormalDistributionGGX(float SqRoughness, float NdotH)
+{
+	static const float PI = 3.14159265359f;
+
+	float SqRoughness2 = SqRoughness * SqRoughness;
+	float Denominator = NdotH * NdotH * (SqRoughness2 - 1.0f) + 1.0f;
+	Denominator *= Denominator;
+	Denominator *= PI;
+
+	return SqRoughness2 / Denominator;
+}
+//---------------------------------------------------------------------
+
+// Dot products must be saturated
+float3 SpecularCookTorrance(float3 SpecularColor, float SqRoughness, float NdotV, float NdotL, float NdotH, float VdotH)
+{
+	float D = NormalDistributionGGX(SqRoughness, NdotH);
+	float G = GeometricSmithSchlickGGX(SqRoughness, NdotV, NdotL);
+	float3 F = FresnelSchlick(SpecularColor, VdotH);
+	return (D * G * F) / (4.0f * NdotL * NdotV);
+	//return (D * G * F) / (3.9999f * NdotL * NdotV + 0.0001f);
+}
+//---------------------------------------------------------------------
--- Content/Src/Shaders/USM/PBR.hlsl
+++ Content/Src/Shaders/USM/PBR.hlsl
@@ -145,6 +145,9 @@ float4 PSMain(PSInSimple In): SV_Target
 	float3 N = PerturbNormal(In.Normal, -In.View, In.UV);
 	float3 V = normalize(In.View);
 
+	float Roughness = 0.9f;
+	float SqRoughness = Roughness * Roughness;
+
 	float3 LightColor = float3(0, 0, 0);
 	for (uint i = 0; i < In.LightInfo[0]; ++i)
 	{
@@ -155,47 +158,46 @@ float4 PSMain(PSInSimple In): SV_Target
 		if (CurrLight.Type == LIGHT_TYPE_DIR)
 		{
 			L = CurrLight.InvDirection;
-			//Intensity = DiffuseLambert(N, L);
-			Intensity = DiffuseOrenNayar(N, L, V, 0.5f);
+			Intensity = DiffuseOrenNayar(N, L, V, SqRoughness);
 		}
 		else
 		{
 			L = CurrLight.Position - In.PosWorld;
-			float DistanceToLight = length(L);
-			L /= DistanceToLight;
-			//Intensity = DiffuseLambert(N, L) * Attenuation(DistanceToLight, CurrLight.InvRange);
-			Intensity = DiffuseOrenNayar(N, L, V, 0.5f) * Attenuation(DistanceToLight, CurrLight.InvRange);
+
+			float SqDistanceToLight = dot(L, L);
+			L *= rsqrt(SqDistanceToLight);
+			//???!!!pre-square inv. range on CPU if no other uses? if there are, pass both InvRange and SqInvRange!
+			float Attenuation = saturate(1.f - SqDistanceToLight * CurrLight.InvRange * CurrLight.InvRange);
+
+			//float DistanceToLight = length(L);
+			//L /= DistanceToLight;
+			//float Attenuation = AttenuationFunction(DistanceToLight, CurrLight.InvRange);
+
+			Intensity = DiffuseOrenNayar(N, L, V, SqRoughness) * Attenuation;
 			if (CurrLight.Type == LIGHT_TYPE_SPOT)
-				Intensity *= SpotlightFalloff(dot(CurrLight.InvDirection, L), CurrLight.Params.x, CurrLight.Params.y);
+				Intensity *= SpotlightFalloffFunction(dot(CurrLight.InvDirection, L), CurrLight.Params.x, CurrLight.Params.y);
 		}
 
-		LightColor += CurrLight.Color * Intensity;
+		float3 SpecularAtNormalIncidence = 0.03f.xxx;
+		float3 H = normalize(L + V);
+		float NdotV = saturate(dot(N, V));
+		float NdotL = saturate(dot(N, L));
+		float NdotH = saturate(dot(N, H));
+		float VdotH = saturate(dot(V, H));
+		float3 SpecularColor = SpecularCookTorrance(SpecularAtNormalIncidence, SqRoughness, NdotV, NdotL, NdotH, VdotH);
 
-		/*
-		Result += Intensity * LightColor[i] * (SurfaceDiffuse + SpecularPhong(N, L, V) * MtlSpecular);
-		*/
+		LightColor += CurrLight.Color * Intensity;
 	}
 
 	return TexAlbedo.Sample(LinearSampler, In.UV) * MtlDiffuse * float4(LightColor, 1);
 }
 //---------------------------------------------------------------------
 
-/*
-float4 PSMainAlphaTest(PSInSimple In): SV_Target
-{
-	float4 Albedo = TexAlbedo.Sample(LinearSampler, In.UV);
-	clip(Albedo.a - 0.5);
-	return Albedo * MtlDiffuse;
-}
-//---------------------------------------------------------------------
-*/
-
 //!!!see old code version for more code!
 float4 PSMainSplatted(PSInSplatted In): SV_Target
 {
 	float2 UV = In.PosWorld.xz * PSCDLODParams.WorldToHM.xy + PSCDLODParams.WorldToHM.zw;
 	float3 TexDiffuse = Splatting(SplatMap.Sample(SplatSampler, UV), In.SplatDetUV.xy, LinearSampler);
-	return  float4(TexDiffuse, 1.f);
-	//return float4(frac(0.6 * In.PosWorld.y), frac(0.6 * In.PosWorld.y), frac(0.6 * In.PosWorld.y), 1.f);
+	return float4(TexDiffuse, 1.f);
 }
 //---------------------------------------------------------------------
